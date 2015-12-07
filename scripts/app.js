7d25719f497f4fbb61d41fa28ba76c1782b05c8f
chrisgrovers <chrisgrovers@gmail.com>
2015-12-06 23:21:32 -0800

    [style] removed commented out code

--- scripts/app.js
+++ scripts/app.js
@@ -7,17 +7,12 @@ app.controller('2048Controller', function(gameFactory) {
   this.game = gameFactory;
 
   this.newGame = function() {
-    // KeyboardService.init();
     this.game.newGame();
-    // this.startGame();
   }
 
   this.startGame = function() {
     var self = this;
     console.log('starting new game');
-    // KeyboardService.on(function(key) {
-    //   self.game.move(key);
-    // })
   }
 
   this.game.newGame();
